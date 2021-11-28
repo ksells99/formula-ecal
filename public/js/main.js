a12ea34a6dc95ea545e32ae05b43bef2f938f4d1
Kai Sells <kai.sells@hotmail.co.uk>
2021-11-28 17:24:27 +0000

    Styling tweaks

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,9 @@
 const racesElement = document.querySelector("#racesAccordion");
 
-let loading = false;
-
 const getRacesFromNotion = async () => {
-  loading = true;
-  const res = await fetch("https://formula-e-cal.herokuapp.com/races");
+  const res = await fetch(`https://formula-e-cal.herokuapp.com/races`);
+  // const res = await fetch(`http://localhost:5000/races`);
   const data = await res.json();
-  loading = false;
   return data;
 };
 
@@ -20,14 +17,31 @@ const addRacesToDOM = async () => {
     newDiv.innerHTML = `
     
       <h2 class="accordion-header bg-primary" id="flush-heading${race.round}">
-        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${race.round}" aria-expanded="false" aria-controls="flush-collapse${race.round}">
-        <img src="./assets/${race.country_code}.png" alt="" style="height: 20px" class="me-2" /> ${race.title}
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${
+          race.round
+        }" aria-expanded="false" aria-controls="flush-collapse${race.round}">
+        <img src="./assets/${
+          race.country_code
+        }.png" alt="" style="height: 20px" class="me-2" />
+        <div class="race-header w-100 d-flex flex-column flex-sm-row justify-content-sm-between">
+          <span>${race.title}</span>
+          <span class="race-date me-2">${dayjs(race.date).format(
+            "DD/MM/YYYY"
+          )}</span>
+        </div>
+        
         </button>
       </h2>
-      <div id="flush-collapse${race.round}" class="accordion-collapse collapse" aria-labelledby="flush-heading${race.round}" data-bs-parent="#racesAccordion">
+      <div id="flush-collapse${
+        race.round
+      }" class="accordion-collapse collapse" aria-labelledby="flush-heading${
+      race.round
+    }" data-bs-parent="#racesAccordion">
         <div class="accordion-body">
+        
         <p>Round ${race.round}</p>
         <p>${race.circuit}, ${race.country}</p>
+        
         </div>
       </div>
     
